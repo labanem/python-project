96eb5ad1d27e1df73f0f68b55926aa4c59c624e3
labanem <labanem@yahoo.com>
2023-09-01 15:25:47 +0300

    Create first.py

---
+++ first.py
@@ -0,0 +1,16 @@
+from math import *
+
+character_name = "Rita Rosemary"
+character_age = 35
+character2_age = 14
+print("Hello World")
+print("This is my 'First Project'. \nIt will be a great experience")
+print("My name is " + character_name)
+print("I am " + str(character_age))
+print(character_age + character2_age)
+
+print(floor(2.3))
+print(ceil(2.7))
+
+user_name = input("Please Enter your name: ")
+print("Hi "+ user_name)
