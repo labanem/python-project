6e32a4debd27dd8b0ef028655d8bec9b5ca47729
labanem <labanem@yahoo.com>
2023-09-01 16:13:46 +0300

    Created file games-demo

---
+++ games-demo.py
@@ -0,0 +1,7 @@
+colour_name = input("Please Enter a colour:")
+plural_noun = input("Please Enter a plural Noun:")
+celebrity_name = input("Please Enter a celebrity name:")
+
+print("Roses are " + colour_name)
+print(plural_noun + " are blue,")
+print("I love " + celebrity_name)
