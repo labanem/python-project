5eb145169855c0bbbdec80a356cf9c68033060c1
labanem <labanem@yahoo.com>
2023-09-05 22:30:05 +0300

    Modified games2-demo

--- games2-demo.py
+++ games2-demo.py
@@ -1,16 +1,27 @@
 i = 1
-while i <= 10:
+while i <= 5:
     print(i)
     i = i + 1
 
 print("Done with the loop")
 
 # Guessing game
+#secret_word = "monopoly"
+#guess = ""
+#while guess != secret_word:
+#    guess = input("Enter guess: ")
+#print("You won!")
 
-secret_word = "monopoly"
+# Advanced guessing game
+
+correct_guess = "antelope"
 guess = ""
+guess_count = 0
+guess_limit = 3
 
-while guess != secret_word:
-    guess = input("Enter guess: ")
-
-print("You won!")
+while guess != correct_guess and guess_limit != 3:
+    if guess_count <= guess_limit and guess != correct_guess:
+        guess = input("Enter guess: ")
+        guess_count += 1
+    else:
+        print("We won!")
