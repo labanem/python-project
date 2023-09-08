a54ed779c45339a57a559febf8537d0756ca4820
labanem <labanem@yahoo.com>
2023-09-09 01:45:20 +0300

    Modified Student Class in Student.py and game5

--- Student.py
+++ Student.py
@@ -8,6 +8,11 @@ class Student:
         self.gpa = gpa
         self.is_on_probation = is_on_probation
 
+    def on_honour_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
 
 #Creating a Car Class
 
--- game5.py
+++ game5.py
@@ -7,6 +7,7 @@ student1 = Student("Jim", "Business", 3.1, False)
 student2 = Student("Oscar", "CPA", 4.0, True)
 
 print(student2.is_on_probation)
+print(student1.on_honour_roll())
 
 try:
     for particulars in Student:
@@ -28,12 +29,14 @@ question_prompts = [
     "What is your favourite food?\n(a) Mashed Potatoes\n(b) Pilau\n(c) Matoke\n\n",
     "What is your favourite car?\n(a) BMW\n(b) Fuso\n(c) Mercedes\n\n",
     "Which programming language is your favourite?\n(a) Java\n(b) Python\n(c) C++\n\n",
+    "What certification are you taking first?\n(a) Oracle DBA\n(b) DevOps Enginner\n(c) MS Azure\n\n"
 ]
 
 questions = [
     Question(question_prompts[0], "a"),
     Question(question_prompts[1], "c"),
-    Question(question_prompts[2], "b")
+    Question(question_prompts[2], "b"),
+    Question(question_prompts[3], "b")
 ]
 
 def ask_questions(questions):
