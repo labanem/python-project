5a91869a18f5d138dcd712b21a5c56681c20d9d8
labanem <labanem@yahoo.com>
2023-09-09 15:31:59 +0300

    Created game6.py

---
+++ game6.py
@@ -0,0 +1,14 @@
+# Inheritance
+from Student import Chef
+from Student import ChineseChef
+
+my_chef = Chef()
+
+my_chef.makes_dessert()
+my_chef.makes_mukimo()
+
+chinese_chef = ChineseChef()
+
+chinese_chef.makes_pilau()
+chinese_chef.makes_fried_rice()
+chinese_chef.makes_dessert()
