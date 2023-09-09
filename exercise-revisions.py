ae358e4b92d760eb6a3ab992a32bf84c8b9b236c
labanem <labanem@yahoo.com>
2023-09-09 19:00:33 +0300

    Advanced calculator v2 using if statement and function

--- exercise-revisions.py
+++ exercise-revisions.py
@@ -1,17 +1,26 @@
 #Advanced Calculator version 2
+
 def calculate(num1,num2,operator):
-    try:
-        if operator == "+":
-            return float(num1) + float(num2)
-        elif operator == "-":
-            return  float(num1) - float(num2)
-        elif operator == "/":
-            return float(num1) / float(num2)
-    except ValueError as err:
-        print(err)
-        
+    if operator != "+" and operator != "-" and operator != "/":
+        print(operator + " is not a valid operator.")
+        return
+    else:
+        try:
+            if operator == "+":
+                return float(num1) + float(num2)
+            elif operator == "-":
+                return  float(num1) - float(num2)
+            elif operator == "/":
+                return float(num1) / float(num2)
+        except ValueError as err:
+            print(err)
+        except ZeroDivisionError as err:
+            print(err)
+
 num1 = input ("Enter value of num1: ")
 operator = input("Enter operator: ")
 num2 = input("Enter value of num2: ")
+
+#print(calculate(num1,num2,operator))
 
 print("The answer is: " + str(calculate(num1,num2,operator)))
