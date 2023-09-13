417c43f81c27fa264173e9940d68137756023448
labanem <labanem@yahoo.com>
2023-09-13 14:54:57 +0300

    Exercise questions 3 and 4

--- done-revisions.py
+++ done-revisions.py
@@ -204,3 +204,25 @@ def ask_questions(questions):
     print(str(score) + "/" + str(len(questions)))
 
 ask_questions(questions)
+
+# 1 A program to print if the given number is odd or even
+
+num = int(input("Enter number: "))
+
+if (num%2) == 0:
+    print("Even number")
+    print("{0} is an even".format(num))
+else:
+    print("{0} is an odd number".format(num))
+
+# 2 A program to find if the given number is positive or negative
+try:
+    num1 = int(input("Please enter number: "))
+    if num1 < 0:
+        print("{0} is negative".format(num1))
+    elif num1 == 0:
+        print("Zero")
+    else:
+        print("{0} is positive".format(num1))
+except ValueError as err:
+    print(err)
--- exercise-revisions.py
+++ exercise-revisions.py
@@ -1,28 +1,20 @@
-# 1 A program to print if the given number is odd or even
-
-num = int(input("Enter number: "))
-
-if (num%2) == 0:
-    print("Even number")
-    print("{0} is an even".format(num))
-else:
-    print("{0} is an odd number".format(num))
-
-# 2 A program to find if the given number is positive or negative
-try:
-    num1 = int(input("Please enter number: "))
-    if num1 < 0:
-        print("{0} is negative".format(num1))
-    elif num1 == 0:
-        print("Zero")
-    else:
-        print("{0} is positive".format(num1))
-except ValueError as err:
-    print(err)
-
 # 3 Sum of two numbers
 
 num2 = int(input("Enter 1st number: "))
 num3 = int(input("Enter 2nd number: "))
 
-print(num2 + num3)
+print("Sum is",num2 + num3)
+
+# 4 To find whether a number is a prime number or not
+
+num4 = int(input("Enter number to check: "))
+flag = False
+if num4 > 1:
+    for i in range(2, num4):
+        if (num4 % i) == 0:
+            flag = True
+        break
+if flag:
+    print("{0} is NOT a prime number.".format(num4))
+else:
+    print("{0} is a prime number".format(num4))
