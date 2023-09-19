2b3cf04fce5ffa87a5c5234a5abd8ad3270737ef
labanem <labanem@yahoo.com>
2023-09-19 10:20:27 +0300

    Qn 10,11,12 exercise-revisions.py

--- done-revisions.py
+++ done-revisions.py
@@ -247,3 +247,39 @@ if flag:
     print("{0} is NOT a prime number.".format(num4))
 else:
     print("{0} is a prime number".format(num4))
+
+    # 5 Determine whether a number is a palindrome or not
+# ie. Remains the same when the digits are reversed e.g., 16461
+
+# num5 = int(input("Enter numer to check for palindrome: "))
+# I'll circle back to this question
+
+# 6 check if a given number is Armstrong or not
+#  I'll circle back to this question
+
+# 7 Anagram
+# To circle back to it
+
+# 8 To find a maximum of 2 numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num1 > num2:
+    print(num1, "is greater")
+elif num2 > num1:
+    print("{0} is greater".format(num2))
+else:
+    print("The numbers are equal")
+
+# 9 To find the minimum of 2 numbers
+
+def minimum(num3, num4):
+    if num3 <= num4:
+        return num3
+    else:
+        return num4
+    
+num3 = float(input("Enter a number: "))
+num4 = float(input("Enter a second number: "))
+
+print(minimum(num3,num4), "is the minimum")
--- exercise-revisions.py
+++ exercise-revisions.py
@@ -1,35 +1,48 @@
-# 5 Determine whether a number is a palindrome or not
-# ie. Remains the same when the digits are reversed e.g., 16461
+# 10 To find a maximum of 3 numbers
 
-# num5 = int(input("Enter numer to check for palindrome: "))
-# I'll circle back to this question
-
-# 6 check if a given number is Armstrong or not
-#  I'll circle back to this question
-
-# 7 Anagram
-# To circle back to it
-
-# 8 To find a maximum of 2 numbers
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter the second number: "))
-
-if num1 > num2:
-    print(num1, "is greater")
-elif num2 > num1:
-    print("{0} is greater".format(num2))
-else:
-    print("The numbers are equal")
-
-# 9 To find the minimum of 2 numbers
-
-def minimum(num3, num4):
-    if num3 <= num4:
-        return num3
+largest = ""
+def max(a,b,c):
+    if (a >= b) and (a >= c):
+        largest = a
+    elif (b >= a) and (b >= c):
+        largest = b
     else:
-        return num4
-    
-num3 = float(input("Enter a number: "))
-num4 = float(input("Enter a second number: "))
+        largest = c
+    return largest
 
-print(minimum(num3,num4), "is the minimum")
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+
+print(max(a,b,c))
+
+# 11 To find minimum of 3 numbers
+
+def min(d,e,f):
+    if (d <= e) and (d <= f):
+        lowest = d
+    elif (e <= d) and (e <= f):
+        lowest = e
+    else:
+        lowest = f
+    return lowest
+
+print(min(20,322,24))
+
+# 12 To find the factorial of a number
+
+num1 = int(input("Enter a number to find its factorial: "))
+
+def findfactorial(num1):
+    factorial = 1
+    if num1 < 0:
+        print("We cannot get factorials of negative integers")
+    elif num1 == 0:
+        print("The factorial of 0 is 1.")
+    else:
+        for i in range(1,num1+1):
+            factorial = factorial * i
+        return factorial
+
+print(findfactorial(num1), "is the factorial of",num1)
+
