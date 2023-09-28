06c7341cd809548dc37b50af6755ed48fca1987f
labanem <labanem@yahoo.com>
2023-09-28 08:31:22 +0300

    Qn 18 and 19

--- done-revisions.py
+++ done-revisions.py
@@ -370,3 +370,65 @@ if(gcd(a,b)):
     print("GCD of",a,"and",b,"is", gcd(a, b))
 else:
     print("Not found!")
+
+# 15 A program to print a right-angled triangled image with *
+def myfunc(n):
+    for i in range(0,n):
+        for j in range(0,i+1):
+            print("* ",end="")
+        print("\r")
+
+n = 5
+myfunc(n)
+
+# 16 A program to print a different type of triangle with *s
+
+def myfunc(n):
+    k = n - 1
+    for i in range(0,n):
+        for j in range(0,k):
+            print(end=" ")
+        k = k - 1
+        for j in range(0, i+1):
+            print("* ", end="")
+        print("\r")
+    
+n = 5
+myfunc(n)
+
+# 17
+def num(n):
+    num = 1
+    for i in range(0,n):
+        num = 1
+        for j in range(0, i+1):
+            print(num, end=" ")
+            num = num + 1
+        print("\r")
+
+n = 5
+num(n)
+
+#rev 17
+def num(n):
+    num = 1
+    for i in range(0,n):
+        num = 1
+        for j in range(0,i+1):
+            print(num,end=" ")
+            num += 1
+        print("\r")
+
+n = 6
+num(6)
+
+#rev 15
+
+def stars(n):
+    for i in range(0,n):
+        for j in range(0,i+1):
+            print("* ",end=" ")
+        print("\r")
+
+n = 7
+stars(7)
--- exercise-revisions.py
+++ exercise-revisions.py
@@ -1,61 +1,37 @@
-# 15 A program to print a right-angled triangled image with *
-def myfunc(n):
-    for i in range(0,n):
-        for j in range(0,i+1):
-            print("* ",end="")
-        print("\r")
-
-n = 5
-myfunc(n)
-
-# 16 A program to print a different type of triangle with *s
-
-def myfunc(n):
-    k = n - 1
-    for i in range(0,n):
-        for j in range(0,k):
-            print(end=" ")
-        k = k - 1
-        for j in range(0, i+1):
-            print("* ", end="")
-        print("\r")
-    
-n = 5
-myfunc(n)
-
-# 17
+# Qn 18
 def num(n):
     num = 1
     for i in range(0,n):
-        num = 1
-        for j in range(0, i+1):
-            print(num, end=" ")
-            num = num + 1
-        print("\r")
-
-n = 5
-num(n)
-
-#rev 17
-def num(n):
-    num = 1
-    for i in range(0,n):
-        num = 1
         for j in range(0,i+1):
             print(num,end=" ")
             num += 1
         print("\r")
 
-n = 6
-num(6)
+n = 5
+num(5)
 
-#rev 15
+# Qn 19
 
-def stars(n):
+alphabet = [ "A", "B", "C", "D", "E"]
+
+def display_alphabet(alphabet):
+    for i in range(len(alphabet)):
+        for j in range(0,i+1):
+            print(alphabet[i],end=" ")
+        print("\r")
+         
+display_alphabet(alphabet)
+
+#Qn 19 using ASCII
+
+def num(n):
+    num = 65
     for i in range(0,n):
         for j in range(0,i+1):
-            print("* ",end=" ")
+            ch = chr(num)
+            print(ch,end=" ")
+            num += 1
         print("\r")
 
-n = 7
-stars(7)
+n = 5
+num(n)
