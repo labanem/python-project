55b79626ae4590a194de0b5b7f46597e22da2ab2
labanem <labanem@yahoo.com>
2024-02-20 18:22:39 +0300

    Creation of Feb revision files

---
+++ done-revisions-Feb-pt2.py
@@ -0,0 +1,12 @@
+
+
+#Determine if a number is an Odd or Even number
+try:
+    num = input("Enter number: ")
+
+    if num%2 == 0:
+        print("{0} is an EVEN number.".format(num))
+    else:
+        print("{0} is an ODD number.".format(num))
+except ValueError as err:
+    print(err)
